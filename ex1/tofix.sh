422c13a3c3dee7ec732d85ff0b4d9e96da36e11d
AlexandreB <barei.alexandre13@gmail.com>
2024-03-20 10:16:47 +0000

    ex

--- ex1/tofix.sh
+++ ex1/tofix.sh
@@ -10,23 +10,24 @@ echo "The value of variable X is: $X"
 echho "Hello, World!"
 
 # 3
-
-if [ $num -eq 10 ; then
+num=10
+if [ "$num" -eq 10 ]  
+then
    echo "Number is 10"
 fi
 
 # 4
 
-echo "Hello, World!'
+echo "Hello, World!"
 
 # 5
 
-for i in (seq 1 1 16)
+for i in $(seq 1 1 16)
 do
-	echo "$i
+	echo "$i"
 done
 
 # 6
 
 Y=8
-echo 'Value of Y is : $Y' 
+echo "Value of Y is : $Y" 
